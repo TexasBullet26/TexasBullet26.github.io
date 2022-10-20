a476cbbce67c3101fc5dbbbca755d96e96cd26b2
Glenn Lanzer <lanzertrey@gmail.com>
2022-10-20 06:04:45 -0500

    Create geo.js

---
+++ assets/js/geo.js
@@ -0,0 +1,18 @@
+const successCallback = (position) => {
+  console.log(position);
+};
+
+const errorCallback = (error) => {
+  console.log(error);
+};
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 10000,
+};
+
+navigator.geolocation.getCurrentPostion(
+  successCallback,
+  errorCallback,
+  options
+);
